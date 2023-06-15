e24ba81a92d0a49d4daebb30389a67eb3222755f
KruII <KruII@users.noreply.github.com>
2023-06-15 04:49:53 +0200

    1

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
 function showInfo() {
-    document.getElementById('output').innerHTML = 'To jest sekcja z informacjami o mnie.';
-  }
+  document.getElementById('output').innerHTML = 'To jest sekcja z informacjami o mnie.';
+}
 
-  function showContact() {
-    document.getElementById('output').innerHTML = 'Możesz się ze mną skontaktować pod adresem e-mail: example@example.com';
-  }
+function showContact() {
+  document.getElementById('output').innerHTML = 'Możesz się ze mną skontaktować pod adresem e-mail: example@example.com';
+}
 
-  function showSkills() {
-    document.getElementById('output').innerHTML = 'Moje umiejętności to: HTML, CSS, JavaScript, itd.';
-  }
+function showSkills() {
+  document.getElementById('output').innerHTML = 'Moje umiejętności to: HTML, CSS, JavaScript, itd.';
+}
 
-  function showInterests() {
-    document.getElementById('output').innerHTML = 'Moje zainteresowania to: programowanie, podróże, sport, itd.';
+function showInterests() {
+  document.getElementById('output').innerHTML = 'Moje zainteresowania to: programowanie, podróże, sport, itd.';
+}
+
+const HeaderT = document.getElementById('HeaderText');
+
+HeaderW("Moja Stronka");
+
+function HeaderW(HeaderWord){
+  HeaderT.innerHTML = '';
+  for (let i = 0; i < HeaderWord.length; i++) {
+    setTimeout(function() {
+      HeaderT.innerHTML += HeaderWord[i];
+    }, i *100)
   }
+}
